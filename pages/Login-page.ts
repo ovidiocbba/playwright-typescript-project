6e1070b6d9a90b6962063657cc111b9dbbc6614e
Ovidio Miranda <ovidiocbba@hotmail.com>
2024-11-03 16:21:49 -0400

    Add Login page

---
+++ pages/Login-page.ts
@@ -0,0 +1,30 @@
+import { expect, type Locator, type Page } from '@playwright/test';
+import baseEnv from '../utils/environmentBaseUrl';
+const env = process.env.ENV!;
+
+export class LoginPage {
+  readonly page: Page;
+  readonly usernameTextField: Locator;
+  readonly passwordTextField: Locator;
+  readonly loginButton: Locator;
+  readonly alertContentLabel: Locator;
+
+
+  constructor(page: Page) {
+    this.page = page;
+    this.usernameTextField = page.locator('input[name=username]');
+    this.passwordTextField = page.locator('input[type=password]');
+    this.loginButton = page.locator('button[type=submit]');
+    this.alertContentLabel = page.locator('.oxd-alert-content > p');
+  }
+
+  async goto() {
+    await this.page.goto(baseEnv[env].home);
+  }
+ 
+  async login(username: string, password: string) {
+    await this.usernameTextField.fill(username);  
+    await this.passwordTextField.fill(password);
+    await this.loginButton.click();
+  }
+}
