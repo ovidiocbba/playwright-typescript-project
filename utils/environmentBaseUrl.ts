be0e88ab3a8fa1e62566fabb2d6019ca17d4dac3
Ovidio Miranda <ovidiocbba@hotmail.com>
2024-11-03 16:23:17 -0400

    Add Enviroment file

---
+++ utils/environmentBaseUrl.ts
@@ -0,0 +1,14 @@
+export default {
+  test: {
+    home: 'https://opensource-demo.orangehrmlive.com/',
+    api: 'https://opensource-demo.orangehrmlive.com/api',
+  },
+  staging: {
+    home: 'https://opensource-demo.orangehrmlive.staging.com/',
+    api: 'https://opensource-demo.orangehrmlive.staging.com/api',
+  },
+  production: {
+    home: 'https://opensource-demo.orangehrmlive.production.com/',
+    api: 'https://opensource-demo.orangehrmlive.production.com/api',
+  }
+};
