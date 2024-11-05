5789581d10aef263c5c5afd36751bd40f624222b
Ovidio Miranda <ovidiocbba@hotmail.com>
2024-11-04 22:05:44 -0400

    Update TC-002

--- tests/Pim/Employee.spec.ts
+++ tests/Pim/Employee.spec.ts
@@ -8,46 +8,46 @@ import { GuidGenerator } from '../../utils/GuidGenerator';
 const userName = process.env.UI_USERNAME!;
 const password = process.env.UI_PASSWORD!;
 
-test('@TC-0001 Verify that an employee can be created', {
+test.describe('PIM', () => {
+  let loginPage: LoginPage;
+  let dashboardPage: DashboardPage;
+  let employeeListPage: EmployeeListPage;
+  let addEmployeePage: AddEmployeePage;
+
+  test.beforeEach(async ({ page }) => {
+    loginPage = new LoginPage(page);
+    dashboardPage = new DashboardPage(page);
+    employeeListPage = new EmployeeListPage(page);
+    addEmployeePage = new AddEmployeePage(page);
+
+    await loginPage.goto();
+    await loginPage.login(userName, password);
+
+    // Navigate to PIM module
+    await dashboardPage.clickPimMenuItem();
+  });
+
+  test('@TC-0001 Verify that an employee can be created', {
     tag: ['@Smoke', '@Functional', '@Regression'],
-  }, async ({ page }) => {
+  }, async () => {
+    await employeeListPage.clickAddButton();
 
-  const loginPage = new LoginPage(page);
-  const dashboardPage = new DashboardPage(page);
-  const employeeListPage = new EmployeeListPage(page);
-  const addEmployeePage = new AddEmployeePage(page);
+    const guid = GuidGenerator.generateNumericGuid(10);
+    await addEmployeePage.addEmployee('Mary', 'Elizabeth', 'Smith', guid);
 
-  await loginPage.goto();
-  await loginPage.login(userName, password);
+    const isEmployeeCreated = await addEmployeePage.verifyEmployeeCreation();
+    expect(isEmployeeCreated).toBeTruthy();
+  });
 
-  await dashboardPage.clickPimMenuItem();
+  test('@TC-0002 Verify that an employee cannot be created when required fields are left empty', {
+    tag: ['@Negative', '@Regression'],
+  }, async () => {
 
-  await employeeListPage.clickAddButton();
+    await employeeListPage.clickAddButton();
 
-  const guid = GuidGenerator.generateNumericGuid(10); // Generate the GUID with the specified
-  await addEmployeePage.addEmployee('Mary', 'Elizabeth','Smith', guid);
+    await addEmployeePage.addEmployee('', '', '', '');
 
-  const isEmployeeCreated = await addEmployeePage.verifyEmployeeCreation();
-  expect(isEmployeeCreated).toBeTruthy();
-});
-
-test('@TC-0002 Verify that an employee cannot be created when required fields are left empty', {
-  tag: ['@Negative', '@Regression'],
-}, async ({ page }) => {
-  const loginPage = new LoginPage(page);
-  const dashboardPage = new DashboardPage(page);
-  const employeeListPage = new EmployeeListPage(page);
-  const addEmployeePage = new AddEmployeePage(page);
-
-  await loginPage.goto();
-  await loginPage.login(userName, password);
-
-  await dashboardPage.clickPimMenuItem();
-
-  await employeeListPage.clickAddButton();
-
-  await addEmployeePage.addEmployee('', '','', '');
-
-  const areErrorsVisible = await addEmployeePage.verifyRequiredFields();
-  expect(areErrorsVisible).toBeTruthy();
+    const areErrorsVisible = await addEmployeePage.verifyRequiredFields();
+    expect(areErrorsVisible).toBeTruthy();
+  });
 });
